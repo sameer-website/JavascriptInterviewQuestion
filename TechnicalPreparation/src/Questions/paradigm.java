270f65f30dfe39aa9d5487df4a5ee2b3d2f6f8ca
sameer <iamsameer6805@gmail.com>
2026-04-12 10:27:14 +0530

    paradigm

--- TechnicalPreparation/src/Questions/paradigm.java
+++ TechnicalPreparation/src/Questions/paradigm.java
@@ -3,8 +3,15 @@ package Questions;
 public class paradigm {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String checkVowels = "mdsAmeerua";
+		int count = 0;
+		for (int i = 0; i < checkVowels.length(); i++) {
+			char string = Character.toLowerCase(checkVowels.charAt(i));
+			if (string == 'a' || string == 'e' || string == 'i' || string == 'o' || string == 'u' ) {
+				count++;	
+			}
+		}
+		System.out.println(count);
 	}
 
 }
