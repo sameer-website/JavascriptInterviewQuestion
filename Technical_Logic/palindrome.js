8bd185828f19cf3640233863700193a75c9253ad
sameer <iamsameer6805@gmail.com>
2026-04-13 13:56:38 +0530

    swaping and palindrome

--- Technical_Logic/palindrome.js
+++ Technical_Logic/palindrome.js
@@ -5,7 +5,7 @@ for (let i = palindrome.length - 1; i >= 0; i--) {
   reverse += palindrome.charAt(i);
 }
 
-if (palindrome === reverse) {
+if (palindrome == reverse) {
   console.log("It is a palindrome");
 } else {
   console.log("Not a palindrome");
---
+++ Technical_Logic/swap.js
@@ -0,0 +1,11 @@
+let a = 80;
+let b = 90;
+let temp;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log("Value of a after swapping: " + a);
+console.log("Value of b after swapping: " + b);
+
